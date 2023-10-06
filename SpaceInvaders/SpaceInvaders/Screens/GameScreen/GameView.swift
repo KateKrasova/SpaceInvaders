a12f7e2b0cdf8367ba3dcda52337fa82f26e3279
Kate Krasova <kate.krasova@icloud.com>
2023-10-06 12:26:31 +0400

    ship shooting

--- SpaceInvaders/SpaceInvaders/Screens/GameScreen/GameView.swift
+++ SpaceInvaders/SpaceInvaders/Screens/GameScreen/GameView.swift
@@ -19,27 +19,33 @@ final class GameView: UIView {
 
     private var props: Props?
 
-    // MARK: - Views
+    private var shipCenterConstraint: Constraint?
+    private var shipCenterOffset = 0.0
 
-    private lazy var rightButton = UIButton(type: .system).do {
-        $0.setImage(UIImage(systemName: "arrowshape.right.fill"), for: .normal)
-        $0.tintColor = .white
-    }
+    // MARK: - Views
 
     private lazy var leftButton = UIButton(type: .system).do {
         $0.setImage(UIImage(systemName: "arrowshape.left.fill"), for: .normal)
         $0.tintColor = .white
+        $0.addTarget(self, action: #selector(leftButtonTap), for: .touchUpInside)
+    }
+
+    private lazy var rightButton = UIButton(type: .system).do {
+        $0.setImage(UIImage(systemName: "arrowshape.right.fill"), for: .normal)
+        $0.tintColor = .white
+        $0.addTarget(self, action: #selector(rightButtonTap), for: .touchUpInside)
     }
 
     private lazy var shotButton = UIButton(type: .system).do {
         $0.setTitle("  SHOT  ", for: .normal)
         $0.backgroundColor = .black
         $0.setTitleColor(Asset.Colors.redMain.color, for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
-        $0.layer.cornerRadius = 12
+        $0.titleLabel?.font = .systemFont(ofSize: Constants.shotButtonFontSize, weight: .bold)
+        $0.layer.cornerRadius = Constants.cornerRadius
         $0.clipsToBounds = true
         $0.borderColor = Asset.Colors.redMain.color
-        $0.borderWidth = 2
+        $0.borderWidth = Constants.shotBorderWidth
+        $0.addTarget(self, action: #selector(shotButtonTap), for: .touchUpInside)
     }
 
     private lazy var shipImage = UIImageView().do {
@@ -49,11 +55,10 @@ final class GameView: UIView {
 
     // MARK: - LifeCycle
 
-    init() {
-        super.init(frame: .zero)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
 
         setup()
-        setupViews()
         setupConstraints()
     }
 
@@ -87,10 +92,6 @@ private extension GameView {
         )
     }
 
-    /// Добавление Views
-    func setupViews() {
-    }
-
     /// Установка констреинтов
     func setupConstraints() {
         leftButton.snp.makeConstraints {
@@ -110,9 +111,60 @@ private extension GameView {
         }
 
         shipImage.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
+            shipCenterConstraint = $0.centerX.equalToSuperview().constraint
+
             $0.bottom.equalTo(shotButton.snp.top).offset(-Constants.offset16)
             $0.height.equalTo(Constants.shipHight)
+        }
+    }
+
+    @objc
+    private func leftButtonTap() {
+        shipCenterOffset -= Constants.centerOffset
+
+        guard abs(shipCenterOffset) <= abs(self.frame.width / 2 - 50.0) else { return }
+
+        shipCenterConstraint?.update(offset: shipCenterOffset)
+    }
+
+    @objc
+    private func rightButtonTap() {
+        shipCenterOffset += Constants.centerOffset
+
+        guard abs(shipCenterOffset) <= abs(self.frame.width / 2 - 50.0) else { return }
+
+        shipCenterConstraint?.update(offset: shipCenterOffset)
+    }
+
+    @objc
+    private func shotButtonTap() {
+        let view = UIView().do {
+            $0.backgroundColor = .white
+            $0.roundCorners(curve: .circular, radius: Constants.cartridgeSize / 2)
+        }
+
+        addSubview(view)
+
+        var pointBottomConstraint: Constraint?
+        var pointBottomOffset = 0.0
+
+        view.snp.makeConstraints {
+            pointBottomConstraint = $0.bottom.equalTo(shipImage.snp.top).constraint
+            $0.centerX.equalToSuperview().offset(shipCenterOffset)
+
+            $0.size.equalTo(CGSize(width: Constants.cartridgeSize, height: Constants.cartridgeSize))
+        }
+
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] timer in
+            guard let self else { return }
+
+            pointBottomOffset -= Constants.pointOffset
+            pointBottomConstraint?.update(offset: pointBottomOffset)
+
+            if abs(pointBottomOffset) > (frame.height - safeAreaInsets.top) {
+                timer.invalidate()
+                view.removeFromSuperview()
+            }
         }
     }
 }
@@ -124,5 +176,11 @@ private extension GameView {
         static let buttonsSize: CGFloat = 35
         static let offset16: CGFloat = 16
         static let shipHight: CGFloat = 50
+        static let cartridgeSize: CGFloat = 5
+        static let shotButtonFontSize: CGFloat = 16
+        static let cornerRadius: CGFloat = 12
+        static let shotBorderWidth: CGFloat = 2
+        static let centerOffset: Double = 10
+        static let pointOffset: Double = 1
     }
 }
